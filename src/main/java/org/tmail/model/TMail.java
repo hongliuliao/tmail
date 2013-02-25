3a47a5a247feceafe836f94b4a8c8d1cb38f5208
hongliuliao <hongliuliao@sohu-inc.com>
2013-02-25 14:12:19 +0800

    如果context为空,则访问html代码

--- src/main/java/org/tmail/model/TMail.java
+++ src/main/java/org/tmail/model/TMail.java
@@ -8,6 +8,7 @@ import java.util.Collections;
 import java.util.List;
 
 import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang.builder.ToStringBuilder;
 import org.apache.commons.lang.builder.ToStringStyle;
 
@@ -30,6 +31,9 @@ public class TMail {
 	 * @return the context
 	 */
 	public String getContext() {
+		if(StringUtils.isBlank(this.context)) {
+			return this.getHtmlContext();
+		}
 		return context;
 	}
 
